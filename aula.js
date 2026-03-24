072264a214361a03389f13b4370a9698c9437fea
victorgsouza6-prog <victor.g.souza6@aluno.senai.br>
2026-03-24 10:09:48 -0300

    Add files via upload

---
+++ aula.js
@@ -0,0 +1,47 @@
+/* const pessoas = {
+    nome: "jão" ,
+    idade: 25,
+    falar: function () {
+        console.log("olá, meu nome é " + this,nome);
+    },
+};
+pessoas.falar()
+console.log(pessoas.nome)
+console.log(pessoas.nome) */
+
+
+/* ///01
+const carro = {
+  marca: "Toyota",
+  modelo: "Corolla",
+  ano: 2022
+};
+console.log(carro); */
+
+/* ///02
+let aluno = {
+  nome: "João",
+  idade: 20
+};
+console.log("Antes:", aluno);
+aluno.idade = 25;
+console.log("Depois:", aluno); */
+
+///03
+/* let produto = {
+  nome: "Notebook",
+  preco: 3500
+};
+
+console.log(`O produto ${produto.nome} custa R$ ${produto.preco}`); */
+
+///04
+let pessoa = {
+  nome: "Maria"
+};
+pessoa.idade = 30;
+console.log(pessoa.nome);
+console.log(pessoa.idade);
+
+console.log(pessoa["nome"]);
+console.log(pessoa["idade"]);
