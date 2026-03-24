a704d8d57fd1d86d2f48239ba9cebe82d066b281
victorgsouza6-prog <victor.g.souza6@aluno.senai.br>
2026-03-24 10:10:42 -0300

    Update aula.js

--- aula.js
+++ aula.js
@@ -1,39 +1,28 @@
-/* const pessoas = {
-    nome: "jão" ,
-    idade: 25,
-    falar: function () {
-        console.log("olá, meu nome é " + this,nome);
-    },
-};
-pessoas.falar()
-console.log(pessoas.nome)
-console.log(pessoas.nome) */
 
-
-/* ///01
+ ///01
 const carro = {
   marca: "Toyota",
   modelo: "Corolla",
   ano: 2022
 };
-console.log(carro); */
+console.log(carro); 
 
-/* ///02
+ ///02
 let aluno = {
   nome: "João",
   idade: 20
 };
 console.log("Antes:", aluno);
 aluno.idade = 25;
-console.log("Depois:", aluno); */
+console.log("Depois:", aluno); 
 
 ///03
-/* let produto = {
+ let produto = {
   nome: "Notebook",
   preco: 3500
 };
 
-console.log(`O produto ${produto.nome} custa R$ ${produto.preco}`); */
+console.log(`O produto ${produto.nome} custa R$ ${produto.preco}`); 
 
 ///04
 let pessoa = {
